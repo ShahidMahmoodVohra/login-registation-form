e60fc7aa6d1ea6b8f0a332bedb144b6f7b36bc2e
ShahidMahmoodVohra <ssmvohra@gmail.com>
2024-10-20 23:50:22 +0500

    changes

--- responsive-login-and-registration-form-start-project/assets/js/main.js
+++ responsive-login-and-registration-form-start-project/assets/js/main.js
@@ -1,4 +1,20 @@
 /*=============== SHOW HIDE PASSWORD LOGIN ===============*/
+const passwordAccess = (loginPass, loginEye) =>{
+    const input = document.getElementById(loginPass),
+          iconEye = document.getElementById(loginEye)  
+
+    iconEye.addEventListener('click' , ()=>{
+        // change password to text
+        input.type === 'password' ? input.type = 'text'
+                                   : input.type = 'password'
+        // icon change  
+        iconEye.classList.toggle('ri-eye-fill')                          
+        iconEye.classList.toggle('ri-eye-off-fill')                          
+
+    });     
+}
+
+passwordAccess('password' , 'loginPassword')
 
 
 /*=============== SHOW HIDE PASSWORD CREATE ACCOUNT ===============*/
